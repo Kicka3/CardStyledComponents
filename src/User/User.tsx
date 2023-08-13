b1d936443ddd2e28643e4232be01d4399c79a579
Kicka3 <kirill_ozornin@mail.ru>
2023-08-13 19:38:26 +0500

    Commit from DevBranch1

--- src/User/User.tsx
+++ src/User/User.tsx
@@ -5,6 +5,8 @@ export const User = () => {
         <div>
             <h1>Юзерссссс</h1>
             <h1>Think about this</h1>
+            <h1>QWEq2222</h1>
+            <h1>33333333333333333333333333333</h1>
         </div>
     )
 }
