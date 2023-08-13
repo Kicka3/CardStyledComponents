82ec27c59004c1d759d45aff16a2e6c02302edb4
Kicka3 <kirill_ozornin@mail.ru>
2023-08-13 18:15:38 +0500

    Add for delete

--- src/User/User.tsx
+++ src/User/User.tsx
@@ -4,6 +4,9 @@ export const User = () => {
     return (
         <div>
             <h1>Users3333</h1>
+            <h1>Users3333</h1>
+            <h1>Users3333</h1>
+            <h1>Users3333</h1>
         </div>
     )
 }
