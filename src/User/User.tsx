58d24086f9569cb5aa2d28b52d197a868ad8ebe7
Kicka3 <kirill_ozornin@mail.ru>
2023-08-13 18:49:46 +0500

    Commit 3

--- src/User/User.tsx
+++ src/User/User.tsx
@@ -3,7 +3,8 @@ import React from "react";
 export const User = () => {
     return (
         <div>
-            <h1><Юзерссссс/h1>
+            <h1>Юзерссссс</h1>
+            <h1>Think about this</h1>
         </div>
     )
 }
